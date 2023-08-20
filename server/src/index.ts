50aa3fdf4c772838b42a643940d4143e9f6a55a2
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2023-08-20 20:18:12 +0530

    Implement apollo server graphql in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,30 @@
 import express, { Request, Response } from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 import dotenv from "dotenv";
+import { readFileSync } from "fs";
+
+const typeDefs = readFileSync("./src/schema.graphql", { encoding: "utf-8" });
 
 const app = express();
 dotenv.config();
 
-const PORT: string = process.env.PORT || "4000";
+const PORT: string = process.env.PORT || "3003";
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Storeship API");
-});
+const resolvers = {
+  Query: {
+    getUser: () => [],
+  },
+};
 
-app.listen(PORT, () => console.log("Server has started on port", PORT));
+async function startServer() {
+  const graphqlServer = new ApolloServer({ resolvers, typeDefs });
+  await graphqlServer.start();
+
+  app.use(express.json());
+  app.use("/graphql", expressMiddleware(graphqlServer));
+
+  app.listen(PORT, () => console.log("Server has started on port", PORT));
+}
+
+startServer();
