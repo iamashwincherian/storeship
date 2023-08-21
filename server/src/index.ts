8800d30a3ca97961b5bc56d42ddab6cda9eaa2f0
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2023-08-21 22:32:57 +0530

    Refactor

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import dotenv from "dotenv";
